49c76cec9fc7eea2aaf256d89fafc4a0c2e99809
Jose Mª Izquierdo Galiot <igaliot21@gmail.com>
2020-08-03 09:38:44 +0200

    plantillas I

--- project1/views.py
+++ project1/views.py
@@ -1,12 +1,17 @@
 from django.http import HttpResponse
+from django.template import Template, Context
 import datetime
 
 def helloWorld(request):
-    return HttpResponse("Hello world")
+    fichero_plantilla=open("project1/templates/helloworld.html") 
+    tmpl_helloWorld=Template(fichero_plantilla.read())
+    fichero_plantilla.close()
+    ctx=Context()
+    documento=tmpl_helloWorld.render(ctx)
+    return HttpResponse(documento)
 
 def getData(request):
     fechaActual=datetime.datetime.now()
-    #documento= "<html><body><h3>Fecha y hora actuales " + fechaActual + "</h3> </body></html>"
     documento= "<html><body><h3>Fecha y hora actuales %s </h3> </body></html>" % fechaActual
     return HttpResponse(documento) 
 
